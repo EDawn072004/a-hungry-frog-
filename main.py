14370cc46f5e6e401b5d86adf2e7138341474a3e
EDawn072004 <69882381+EDawn072004@users.noreply.github.com>
2020-08-27 11:42:56 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,101 +1,127 @@
+# killing rivals
+
+def on_a_pressed():
+    global spit
+    spit = sprites.create_projectile_from_sprite(img("""
+            . . . . . 
+                    1 1 1 1 . 
+                    . 1 1 1 1 
+                    1 1 1 1 . 
+                    . . . . .
+        """),
+        Frog,
+        50,
+        0)
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
+# eat cheez it
+
+def on_on_overlap(sprite, otherSprite):
+    otherSprite.destroy()
+    if info.life() < 10:
+        info.change_life_by(1)
+    
+    
+sprites.on_overlap(SpriteKind.player, SpriteKind.food, on_on_overlap)
+
+def on_on_overlap2(sprite, otherSprite):
+    sprite.destroy()
+    otherSprite.destroy()
+    info.change_score_by(1)
+sprites.on_overlap(SpriteKind.projectile, SpriteKind.enemy, on_on_overlap2)
+
+# lose life
+
+def on_on_overlap3(sprite, otherSprite):
+    otherSprite.destroy()
+    info.change_life_by(-1)
+sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, on_on_overlap3)
+
+rivals: Sprite = None
+Cheez_it: Sprite = None
+spit: Sprite = None
+Frog: Sprite = None
 # setup
 scene.set_background_color(9)
 info.set_life(1)
 info.set_score(0)
-
 # Frog
 Frog = sprites.create(img("""
-    . . . . . . . . . . . . . . 7 7 . . . .
-    . . . . . . . . . . . . . 7 7 7 7 . . .
-    . . . . . . . . . . . . 7 7 7 1 1 . . .
-    . . . . . . . 7 7 7 7 7 7 7 7 1 f . . .
-    . . . . . . 7 7 7 7 7 7 7 7 7 1 f . . .
-    . . . . . 7 7 7 7 7 7 7 7 7 7 1 f . . .
-    . . . . 7 7 7 7 7 7 7 7 7 7 7 1 1 . . .
-    . . . . 7 7 7 7 7 7 7 7 7 7 7 7 7 . . .
-    . . . . 7 7 7 7 7 7 7 7 7 7 7 7 7 . . .
-    . . . . 7 7 7 7 7 7 7 7 7 f f f f . . .
-    . . . . 7 7 7 7 7 7 7 7 f . . . . . . .
-    . . . . 7 7 7 7 7 7 7 7 f . . . . . . .
-    . . . . 7 7 7 7 7 7 7 7 7 f f f f . . .
-    . . . . . 7 7 7 7 7 7 7 7 7 7 7 . . . .
-    . . . . . . 7 7 7 7 7 7 7 7 7 . . . . .
-    . . . . . . . 7 7 7 7 7 7 7 . . . . . .
-    . . . . . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . . . . . .
-"""))
+        . . . . . . . . . . . . . . 7 7 . . . . 
+            . . . . . . . . . . . . . 7 7 7 7 . . . 
+            . . . . . . . . . . . . 7 7 7 1 1 . . . 
+            . . . . . . . 7 7 7 7 7 7 7 7 1 f . . . 
+            . . . . . . 7 7 7 7 7 7 7 7 7 1 f . . . 
+            . . . . . 7 7 7 7 7 7 7 7 7 7 1 f . . . 
+            . . . . 7 7 7 7 7 7 7 7 7 7 7 1 1 . . . 
+            . . . . 7 7 7 7 7 7 7 7 7 7 7 7 7 . . . 
+            . . . . 7 7 7 7 7 7 7 7 7 7 7 7 7 . . . 
+            . . . . 7 7 7 7 7 7 7 7 7 f f f f . . . 
+            . . . . 7 7 7 7 7 7 7 7 f . . . . . . . 
+            . . . . 7 7 7 7 7 7 7 7 f . . . . . . . 
+            . . . . 7 7 7 7 7 7 7 7 7 f f f f . . . 
+            . . . . . 7 7 7 7 7 7 7 7 7 7 7 . . . . 
+            . . . . . . 7 7 7 7 7 7 7 7 7 . . . . . 
+            . . . . . . . 7 7 7 7 7 7 7 . . . . . . 
+            . . . . . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . . . . .
+    """),
+    0)
 Frog.set_position(10, 60)
-Frog.set_flag(SpriteFlag.StayInScreen, True)
+Frog.set_flag(SpriteFlag.STAY_IN_SCREEN, True)
 Frog.set_kind(SpriteKind.player)
-
-# frog movement 
+# frog movement
 controller.move_sprite(Frog, 200, 200)
+# cheez it
 
-# cheez it 
 def on_update_interval():
+    global Cheez_it
     Cheez_it = sprites.create(img("""
-    4 4 4 4 4 4 4 . . . .
-    4 e 4 4 4 e 4 . . . .
-    4 4 4 e 4 4 4 . . . .
-    4 4 4 4 4 4 4 . . . .
-    4 e 4 4 4 e 4 . . . .
-    4 4 4 4 4 4 4 . . . .
-    . . . . . . . . . . .
-    . . . . . . . . . . .
-    . . . . . . . . . . .
-    """))
+            4 4 4 4 4 4 4 . . . . 
+                    4 e 4 4 4 e 4 . . . . 
+                    4 4 4 e 4 4 4 . . . . 
+                    4 4 4 4 4 4 4 . . . . 
+                    4 e 4 4 4 e 4 . . . . 
+                    4 4 4 4 4 4 4 . . . . 
+                    . . . . . . . . . . . 
+                    . . . . . . . . . . . 
+                    . . . . . . . . . . .
+        """),
+        0)
     Cheez_it.set_position(scene.screen_width(), randint(20, 100))
     Cheez_it.set_velocity(-50, 0)
-    Cheez_it.set_kind(SpriteKind.Food)
+    Cheez_it.set_kind(SpriteKind.food)
 game.on_update_interval(1000, on_update_interval)
 
 # rivals
-def on_update2_interval():
+
+def on_update_interval2():
+    global rivals
     rivals = sprites.create(img("""
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . 1 . . . . .
-        . . . . . . . . f 1 1 1 f . . .
-        . . . f f f f . f 1 1 1 f f . .
-        . . . f 1 f f f f 1 1 1 f f . .
-        . . . f f f f f f f f f f f . .
-        . . . f 1 f f f f 1 1 1 f f . .
-        . . . f f f f . f 1 1 1 f f . .
-        . . . . . . . . f 1 1 1 f . . .
-        . . . . . . . . . . 1 . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-    """))
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . 1 . . . . . 
+                    . . . . . . . . f 1 1 1 f . . . 
+                    . . . f f f f . f 1 1 1 f f . . 
+                    . . . f 1 f f f f 1 1 1 f f . . 
+                    . . . f f f f f f f f f f f . . 
+                    . . . f 1 f f f f 1 1 1 f f . . 
+                    . . . f f f f . f 1 1 1 f f . . 
+                    . . . . . . . . f 1 1 1 f . . . 
+                    . . . . . . . . . . 1 . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """),
+        0)
     rivals.set_position(scene.screen_width(), randint(20, 100))
     rivals.set_velocity(-50, 0)
     rivals.set_kind(SpriteKind.enemy)
-game.on_update_interval(1200, on_update2_interval)
+game.on_update_interval(1200, on_update_interval2)
 
-# killing rivals
-def on_event_pressed2():
-    spit = sprites.create_projectile_from_sprite(img("""
-        . . . . .
-        1 1 1 1 .
-        . 1 1 1 1
-        1 1 1 1 .
-        . . . . .
-    """), Frog, 50, 0)
-controller.A.on_event(ControllerButtonEvent.PRESSED, on_event_pressed2)
-def on_overlap2(sprite, otherSprite):
-    sprite.destroy()
-    otherSprite.destroy()
-    info.change_score_by(1)
-sprites.on_overlap(SpriteKind.projectile, SpriteKind.enemy, on_overlap2)
-
-# lose life
-def on_overlap(sprite, otherSprite):
-    otherSprite.destroy()
-    info.change_life_by(-1)
-sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, on_overlap)
 
 
--- main.ts
+++ main.ts
@@ -1,93 +1,108 @@
-//  setup
-scene.setBackgroundColor(9)
-info.setLife(1)
-info.setScore(0)
-//  Frog
-let Frog = sprites.create(img`
-    . . . . . . . . . . . . . . 7 7 . . . .
-    . . . . . . . . . . . . . 7 7 7 7 . . .
-    . . . . . . . . . . . . 7 7 7 1 1 . . .
-    . . . . . . . 7 7 7 7 7 7 7 7 1 f . . .
-    . . . . . . 7 7 7 7 7 7 7 7 7 1 f . . .
-    . . . . . 7 7 7 7 7 7 7 7 7 7 1 f . . .
-    . . . . 7 7 7 7 7 7 7 7 7 7 7 1 1 . . .
-    . . . . 7 7 7 7 7 7 7 7 7 7 7 7 7 . . .
-    . . . . 7 7 7 7 7 7 7 7 7 7 7 7 7 . . .
-    . . . . 7 7 7 7 7 7 7 7 7 f f f f . . .
-    . . . . 7 7 7 7 7 7 7 7 f . . . . . . .
-    . . . . 7 7 7 7 7 7 7 7 f . . . . . . .
-    . . . . 7 7 7 7 7 7 7 7 7 f f f f . . .
-    . . . . . 7 7 7 7 7 7 7 7 7 7 7 . . . .
-    . . . . . . 7 7 7 7 7 7 7 7 7 . . . . .
-    . . . . . . . 7 7 7 7 7 7 7 . . . . . .
-    . . . . . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . . . . . .
-`)
-Frog.setPosition(10, 60)
-Frog.setFlag(SpriteFlag.StayInScreen, true)
-Frog.setKind(SpriteKind.Player)
-//  frog movement 
-controller.moveSprite(Frog, 200, 200)
-//  cheez it 
-game.onUpdateInterval(1000, function on_update_interval() {
-    let Cheez_it = sprites.create(img`
-    4 4 4 4 4 4 4 . . . .
-    4 e 4 4 4 e 4 . . . .
-    4 4 4 e 4 4 4 . . . .
-    4 4 4 4 4 4 4 . . . .
-    4 e 4 4 4 e 4 . . . .
-    4 4 4 4 4 4 4 . . . .
-    . . . . . . . . . . .
-    . . . . . . . . . . .
-    . . . . . . . . . . .
-    `)
-    Cheez_it.setPosition(scene.screenWidth(), randint(20, 100))
-    Cheez_it.setVelocity(-50, 0)
-    Cheez_it.setKind(SpriteKind.Food)
-})
-//  rivals
-game.onUpdateInterval(1200, function on_update2_interval() {
-    let rivals = sprites.create(img`
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . 1 . . . . .
-        . . . . . . . . f 1 1 1 f . . .
-        . . . f f f f . f 1 1 1 f f . .
-        . . . f 1 f f f f 1 1 1 f f . .
-        . . . f f f f f f f f f f f . .
-        . . . f 1 f f f f 1 1 1 f f . .
-        . . . f f f f . f 1 1 1 f f . .
-        . . . . . . . . f 1 1 1 f . . .
-        . . . . . . . . . . 1 . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-    `)
-    rivals.setPosition(scene.screenWidth(), randint(20, 100))
-    rivals.setVelocity(-50, 0)
-    rivals.setKind(SpriteKind.Enemy)
-})
 //  killing rivals
-controller.A.onEvent(ControllerButtonEvent.Pressed, function on_event_pressed2() {
-    let spit = sprites.createProjectileFromSprite(img`
-        . . . . .
-        1 1 1 1 .
-        . 1 1 1 1
-        1 1 1 1 .
-        . . . . .
-    `, Frog, 50, 0)
+controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
+    
+    spit = sprites.createProjectileFromSprite(img`
+            . . . . . 
+                    1 1 1 1 . 
+                    . 1 1 1 1 
+                    1 1 1 1 . 
+                    . . . . .
+        `, Frog, 50, 0)
 })
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function on_overlap2(sprite: Sprite, otherSprite: Sprite) {
+//  eat cheez it
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function on_on_overlap(sprite: Sprite, otherSprite: Sprite) {
+    otherSprite.destroy()
+    if (info.life() < 10) {
+        info.changeLifeBy(1)
+    }
+    
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function on_on_overlap2(sprite: Sprite, otherSprite: Sprite) {
     sprite.destroy()
     otherSprite.destroy()
     info.changeScoreBy(1)
 })
 //  lose life
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function on_overlap(sprite: Sprite, otherSprite: Sprite) {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function on_on_overlap3(sprite: Sprite, otherSprite: Sprite) {
     otherSprite.destroy()
     info.changeLifeBy(-1)
+})
+let rivals : Sprite = null
+let Cheez_it : Sprite = null
+let spit : Sprite = null
+let Frog : Sprite = null
+//  setup
+scene.setBackgroundColor(9)
+info.setLife(1)
+info.setScore(0)
+//  Frog
+Frog = sprites.create(img`
+        . . . . . . . . . . . . . . 7 7 . . . . 
+            . . . . . . . . . . . . . 7 7 7 7 . . . 
+            . . . . . . . . . . . . 7 7 7 1 1 . . . 
+            . . . . . . . 7 7 7 7 7 7 7 7 1 f . . . 
+            . . . . . . 7 7 7 7 7 7 7 7 7 1 f . . . 
+            . . . . . 7 7 7 7 7 7 7 7 7 7 1 f . . . 
+            . . . . 7 7 7 7 7 7 7 7 7 7 7 1 1 . . . 
+            . . . . 7 7 7 7 7 7 7 7 7 7 7 7 7 . . . 
+            . . . . 7 7 7 7 7 7 7 7 7 7 7 7 7 . . . 
+            . . . . 7 7 7 7 7 7 7 7 7 f f f f . . . 
+            . . . . 7 7 7 7 7 7 7 7 f . . . . . . . 
+            . . . . 7 7 7 7 7 7 7 7 f . . . . . . . 
+            . . . . 7 7 7 7 7 7 7 7 7 f f f f . . . 
+            . . . . . 7 7 7 7 7 7 7 7 7 7 7 . . . . 
+            . . . . . . 7 7 7 7 7 7 7 7 7 . . . . . 
+            . . . . . . . 7 7 7 7 7 7 7 . . . . . . 
+            . . . . . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . . . . .
+    `, 0)
+Frog.setPosition(10, 60)
+Frog.setFlag(SpriteFlag.StayInScreen, true)
+Frog.setKind(SpriteKind.Player)
+//  frog movement
+controller.moveSprite(Frog, 200, 200)
+//  cheez it
+game.onUpdateInterval(1000, function on_update_interval() {
+    
+    Cheez_it = sprites.create(img`
+            4 4 4 4 4 4 4 . . . . 
+                    4 e 4 4 4 e 4 . . . . 
+                    4 4 4 e 4 4 4 . . . . 
+                    4 4 4 4 4 4 4 . . . . 
+                    4 e 4 4 4 e 4 . . . . 
+                    4 4 4 4 4 4 4 . . . . 
+                    . . . . . . . . . . . 
+                    . . . . . . . . . . . 
+                    . . . . . . . . . . .
+        `, 0)
+    Cheez_it.setPosition(scene.screenWidth(), randint(20, 100))
+    Cheez_it.setVelocity(-50, 0)
+    Cheez_it.setKind(SpriteKind.Food)
+})
+//  rivals
+game.onUpdateInterval(1200, function on_update_interval2() {
+    
+    rivals = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . 1 . . . . . 
+                    . . . . . . . . f 1 1 1 f . . . 
+                    . . . f f f f . f 1 1 1 f f . . 
+                    . . . f 1 f f f f 1 1 1 f f . . 
+                    . . . f f f f f f f f f f f . . 
+                    . . . f 1 f f f f 1 1 1 f f . . 
+                    . . . f f f f . f 1 1 1 f f . . 
+                    . . . . . . . . f 1 1 1 f . . . 
+                    . . . . . . . . . . 1 . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        `, 0)
+    rivals.setPosition(scene.screenWidth(), randint(20, 100))
+    rivals.setVelocity(-50, 0)
+    rivals.setKind(SpriteKind.Enemy)
 })
